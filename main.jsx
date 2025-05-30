2d79dd5dc30d2ae3754eba8b4c51a0261f83b241
Coastalshine <tristan.coastalshine@gmail.com>
2025-05-30 11:55:14 +0200

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,10 +1,10 @@
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import QuoteForm from "./QuoteForm.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import QuoteForm from './QuoteForm.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QuoteForm />
   </React.StrictMode>
-);
+)
