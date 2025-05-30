428229484f1e7832e2e5ee3457adfc6afd7e05cc
Coastalshine <tristan.coastalshine@gmail.com>
2025-05-30 13:10:42 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import Home from './pages/Home'
+import QuoteForm from './pages/QuoteForm'
+import Contact from './pages/Contact'
+import Projects from './pages/Projects'
+import './index.css'
+
+const App = () => (
+  <BrowserRouter>
+    <nav className="navbar">
+      <NavLink to="/" className="navlink">Home</NavLink>
+      <NavLink to="/projects" className="navlink">Recent Projects</NavLink>
+      <NavLink to="/quote" className="navlink">Get a Quote</NavLink>
+      <NavLink to="/contact" className="navlink">Contact</NavLink>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/quote" element={<QuoteForm />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </BrowserRouter>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 
